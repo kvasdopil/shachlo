d2257fe5fe4a98f2efcb682dcd3e8975834bebbb
Alexey Guskov <lexag@mail.ru>
2018-06-22 13:05:50 +0200

    particle systems
    Signed-off-by: Alexey Guskov <lexag@mail.ru>

---
+++ Assets/ExhaustControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExhaustControl : MonoBehaviour {
+
+	public ParticleSystem m_back;
+	public ParticleSystem[] m_fwd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float m_rotation = Input.GetAxis("Horizontal");
+		float m_movement = Input.GetAxis("Vertical");
+
+		if (m_movement > 0.1) {
+			if (!m_back.isPlaying) {
+				m_back.Play();
+			}
+		} else {
+			if (m_back.isPlaying) {
+				m_back.Stop();
+			}
+		}
+
+		for (int i = 0; i < m_fwd.Length; i++) {
+			bool playing = m_fwd[i].isPlaying;
+			bool shouldPlay = (m_movement < -0.1);
+
+			if (playing && !shouldPlay) {
+				m_fwd[i].Stop();
+			}
+			if (!playing && shouldPlay) {
+				m_fwd[i].Play();
+			}
+		}
+	}
+}
--- Assets/InputControl.cs
+++ Assets/InputControl.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputControl : MonoBehaviour {
-	public GameObject m_player;
 	public float m_rotationSpeed = 100;
 	public float m_maxAcceleration = 100;
 
 	private float m_rotation;
 	private float m_movement;
 
+	private Transform m_transform;
 	private Rigidbody m_body;
-
 
 	// Use this for initialization
 	private void Start () {
-		m_body = m_player.GetComponent<Rigidbody>();
+		m_body = GetComponent<Rigidbody>();
+		m_transform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
@@ -25,11 +25,11 @@ public class InputControl : MonoBehaviour {
 	}
 
 	private void FixedUpdate() {
-		Vector3 angles = m_body.rotation.eulerAngles;
-		m_body.rotation = Quaternion.Euler(angles.x, angles.y, angles.z + m_rotation * m_rotationSpeed * Time.deltaTime);
+		Vector3 angles = m_transform.rotation.eulerAngles;
+		m_transform.rotation = Quaternion.Euler(angles.x, angles.y, angles.z + m_rotation * m_rotationSpeed * Time.deltaTime * -1);
 
 		if (Mathf.Abs(m_movement) > 0.1) {
-			m_body.AddForce(Vector3.up * m_maxAcceleration * m_movement * Time.deltaTime);
+			m_body.AddForce(m_transform.rotation * Vector3.up * m_maxAcceleration * m_movement * Time.deltaTime);
 		}
 	}
 }
